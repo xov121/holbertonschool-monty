c02edc018ee02b6f475a89d7ed27e0487ddd6d65
xov121 <x.o.v121@gmail.com>
2024-01-04 09:18:28 -0400

    Added function prototype for pop.

--- monty.h
+++ monty.h
@@ -42,5 +42,6 @@ void push(monty_stack_t **stack, unsigned int line_number, char *arg);
 void pall(monty_stack_t **stack, unsigned int line_number, char *arg);
 void monty_run(const char *file_name);
 void pint(monty_stack_t **stack, unsigned int line_number, char *arg);
+void pop(monty_stack_t **stack, unsigned int line_number, char *arg);
 
 #endif /* MONTY_H */
