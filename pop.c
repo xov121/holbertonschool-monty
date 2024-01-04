c19b7355bc777a65db8951eca86713b019074146
xov121 <x.o.v121@gmail.com>
2024-01-04 09:18:10 -0400

    Function that implements pop opciode.

---
+++ pop.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * pop - removes the top element of the stack
+ * @stack: double pointer to the top of the stack
+ * @line_number: line number in the script, used for error messaging
+ * @arg: unused
+*/
+void pop(monty_stack_t **stack, unsigned int line_number, char *arg)
+{
+	monty_stack_t *temp;
+
+	printf("Stack is %s\n", (*stack == NULL) ? "empty" : "not empty");
+	(void)arg;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	*stack = (*stack)->next;
+	free(temp);
+}
