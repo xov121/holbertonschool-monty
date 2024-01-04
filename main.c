18693c4e2b130513da93bc7aae30434a0827f3ad
xov121 <x.o.v121@gmail.com>
2024-01-04 08:29:13 -0400

    Resctructured monty_run so it complies to the 40 lines per function rule. Added description for both main and monty_run functions.

--- main.c
+++ main.c
@@ -1,56 +1,59 @@
 #include "monty.h"
 
+/**
+ * monty_run - executes the monty bytecodes from a given file
+ * @file_name: the name of the file containing monty bytecodes
+*/
 void monty_run(const char *file_name)
 {
-	FILE *file;
+	FILE *file = fopen(file_name, "r");
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t read;
 	unsigned int line_number = 0;
 	monty_stack_t *stack = NULL;
-	instruction_t instructions[] =
-	{
-		{"push", push},
-		{"pall", pall},
-		{NULL, NULL}
-	};
+	instruction_t instructions[] = {{"push", push}, {"pall", pall}, {NULL, NULL}};
 
-	file = fopen(file_name, "r");
 	if (!file)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", file_name);
+		fprintf(stderr, "error: Can't open file %s\n", file_name);
 		exit(EXIT_FAILURE);
 	}
 
-	while ((read = getline(&line, &len, file)) != -1)
+	while (getline(&line, &len, file) != -1)
 	{
-		int i;
-		char *token;
+		char *token = strtok(line, " \n\t\r");
+
 		line_number++;
-		token = strtok(line, " \n\t\r");
 		if (token)
 		{
-			for (i = 0; instructions[i].opcode; i++)
+			for (int i = 0; instructions[i].opcode; i++)
 			{
 				if (strcmp(token, instructions[i].opcode) == 0)
 				{
-					instructions[i].f(&stack, line_number, strtok(NULL, " \n\t\r"));
-					break;
-				}
+				instructions[i].f(&stack, line_number, strtok(NULL, " \n\t\r"));
+				break;
 			}
 		}
 	}
-	free(line);
-	fclose(file);
+}
 
-	while (stack)
-	{
-		monty_stack_t *temp = stack;
-		stack = stack->next;
-		free(temp);
+free(line);
+fclose(file);
+
+for (monty_stack_t *temp; stack; stack = temp)
+{
+	temp = stack->next;
+	free(stack);
 	}
 }
 
+/**
+ * main - entry point for the monty interpreter program
+ * @argc: the number of command-line arguments
+ * @argv: array of command-line arguments
+ *
+ * Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
