427e612569c3b3cd4a731b55ab1278c5135b74f0
xov121 <x.o.v121@gmail.com>
2024-01-04 02:44:16 -0400

    Header file that contains the needed prototypes and definitions for the monty project.

---
+++ monty.h
@@ -0,0 +1,43 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+/* Standard Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Data Structures */
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list structure for stack, queues, LIFO, FIFO
+*/
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} monty_stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function for stack, queues, LIFO, FIFO
+*/
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(monty_stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* Function Prototypes */
+int main(void);
+void push(monty_stack_t **stack, unsigned int line_number, char *arg);
+void pall(monty_stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
